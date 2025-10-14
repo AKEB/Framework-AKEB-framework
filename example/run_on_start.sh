471f1a4eefe5164247a024fd7a50144cec475f95
Vadim Babadzhanyan <vadim.babadzhanyan@my.games>
2025-10-14 22:44:26 +0300

    8.4

--- example/run_on_start.sh
+++ example/run_on_start.sh
@@ -6,5 +6,4 @@ cd /app/vendor/akeb/framework/src/ && SERVER_ROOT=/app php migrate.php
 
 cd /app/vendor/akeb/framework/src/crons/ && SERVER_ROOT=/app ./run_all.sh
 
-# cd /app/crons/ && ./run_all.sh
-
+cd /app/crons/ && ./run_all.sh
