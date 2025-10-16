9fdaf45a863145e1c724623cdca803919664fbc6
Vadim Babadzhanyan <vadim.babadzhanyan@my.games>
2025-10-16 16:46:23 +0300

    Fix PHP Unit Test for Developers

--- example/run_on_start.sh
+++ example/run_on_start.sh
@@ -6,7 +6,6 @@ cd /app/
 if [ "$DEVELOPMENT" = "true" ] && [ -d "/app_framework" ]; then
   echo "Development mode: using local framework repository."
   composer config repositories.framework path /app_framework
-  rm -rf /app/vendor/ /app/composer.lock
   cd /app && composer install
 else
   echo "Production mode: local /app_framework directory not found. Relying on composer.json for remote repository."
