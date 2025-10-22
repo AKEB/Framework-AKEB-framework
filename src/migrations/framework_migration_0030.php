8677c9e895fc8d7c39cee1fd7fd6d4e78db720c7
Vadim Babadzhanyan <vadim.babadzhanyan@my.games>
2025-10-22 15:27:29 +0300

    no message

--- src/migrations/framework_migration_0030.php
+++ src/migrations/framework_migration_0030.php
@@ -6,6 +6,22 @@ class Framework_Migration_0030 {
 
 	static function install() {
 		global $db;
+
+		$result = [];
+		$db->db_GetQueryArray("SELECT `id` FROM `permissions` WHERE `permission` IN (
+			'".\Permissions::MANAGE_USERS."',
+			'".\Permissions::MANAGE_USER_PERMISSIONS."',
+			'".\Permissions::MANAGE_USER_GROUPS."',
+			'".\Permissions::IMPERSONATE_USER."',
+			'".\Permissions::MANAGE_GROUPS."',
+			'".\Permissions::MANAGE_GROUP_PERMISSIONS."'
+		);", $result);
+		$permissions_ids = [];
+		foreach($result as $item) {
+			$permissions_ids[] = intval($item['id']);
+		}
+
+		$db->execSQL("DELETE FROM `translates` WHERE `table` = 'permissions' AND `field_id` IN (".implode(',', $permissions_ids).");");
 		$db->execSQL("DELETE FROM `permissions` WHERE `permission` IN (
 			'".\Permissions::MANAGE_USERS."',
 			'".\Permissions::MANAGE_USER_PERMISSIONS."',
@@ -22,12 +38,31 @@ class Framework_Migration_0030 {
 			INSERT INTO `permissions`
 				(`id`, `permission`)
 			VALUES
-				(NULL, '".\Permissions::MANAGE_USERS."'),
-				(NULL, '".\Permissions::MANAGE_USER_PERMISSIONS."'),
-				(NULL, '".\Permissions::MANAGE_USER_GROUPS."'),
-				(NULL, '".\Permissions::MANAGE_GROUPS."'),
-				(NULL, '".\Permissions::MANAGE_GROUP_PERMISSIONS."'),
-				(NULL, '".\Permissions::IMPERSONATE_USER."')
+				(2, '".\Permissions::MANAGE_USERS."'),
+				(4, '".\Permissions::MANAGE_USER_PERMISSIONS."'),
+				(5, '".\Permissions::MANAGE_USER_GROUPS."'),
+				(6, '".\Permissions::MANAGE_GROUPS."'),
+				(8, '".\Permissions::MANAGE_GROUP_PERMISSIONS."'),
+				(9, '".\Permissions::IMPERSONATE_USER."');
+		");
+
+		$db->execSQL("
+			INSERT INTO `translates`
+				(`id`, `table`, `field`, `field_id`, `language`, `value`)
+			VALUES
+				(NULL, 'permissions', 'title', 2, 'ru', 'Управление пользователями'),
+				(NULL, 'permissions', 'title', 4, 'ru', 'Управление правами пользователя'),
+				(NULL, 'permissions', 'title', 5, 'ru', 'Управление группами пользователя'),
+				(NULL, 'permissions', 'title', 6, 'ru', 'Управление группами'),
+				(NULL, 'permissions', 'title', 8, 'ru', 'Управление правами группы'),
+				(NULL, 'permissions', 'title', 9, 'ru', 'Вход под пользователем'),
+
+				(NULL, 'permissions', 'title', 2, 'en', 'Manage Users'),
+				(NULL, 'permissions', 'title', 4, 'en', 'Manage User Permissions'),
+				(NULL, 'permissions', 'title', 5, 'en', 'Manage User Groups'),
+				(NULL, 'permissions', 'title', 6, 'en', 'Manage Groups'),
+				(NULL, 'permissions', 'title', 8, 'en', 'Manage Group Permissions'),
+				(NULL, 'permissions', 'title', 9, 'en', 'Impersonate Users')
 		");
 	}
 }
